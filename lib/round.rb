56e13a51035102af4ed9bd8c83a0bda036fa2a74
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-22 16:05:41 -0600

    Add and pass 2 more tests

--- lib/round.rb
+++ lib/round.rb
@@ -1,5 +1,29 @@
+require './lib/turn'
+
 class Round
+  attr_accessor :deck, :turns, :turn_count,:number_correct
   def initialize(deck)
     @deck = deck
+    @turns = []
+    @turn_count
+    @current_card
+    @number_correct = 0
   end
+
+  def current_card
+    @current_card = deck.cards[0]
+  end
+
+  def take_turn(answer)
+    card = self.current_card
+    turn1 = Turn.new(answer, card)
+    deck.cards.shift
+    turns << turn1
+    if turn1.correct?
+      @number_correct += 1
+    end
+    turn1
+  end
+
+
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -1,20 +1,83 @@
 require './lib/deck'
 require './lib/card'
-require './lib/deck'
+require './lib/turn'
 require './lib/round'
 
-card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-cards = [card1, card2, card3]
 
-deck = Deck.new(cards)
 
 RSpec.describe Round do
+
+  card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+  card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+  card3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+  cards = [card1, card2, card3]
+  deck = Deck.new(cards)
+
   it 'exists' do
     round = Round.new(deck)
   
     expect(round).to be_instance_of(Round)
   end
+
+  it 'has a deck' do
+    round = Round.new(deck)
+  
+    expect(round.deck).to eq(deck)
+  end
+
+  it 'can have turns' do
+    round = Round.new(deck)
+  
+    expect(round.turns).to eq([])
+  end
+
+  it 'has a current flash card' do
+    round = Round.new(deck)
+
+    expect(round.current_card).to eq(deck.cards[0])
+  end
+
+  it 'can take a turn and create turn instance' do
+    round = Round.new(deck)
+    
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn).to be_instance_of(Turn)
+  end
+
+  it 'can be correct' do
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+
+    expect(new_turn.correct?).to be true
+  end
+
+  it 'can remember turns taken' do
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    new_turn = round.take_turn("Juneau")
+
+    expect(round.turns).to eq([new_turn])
+  end
+
+  it 'can store number of correct answers' do
+    cards = [card1, card2, card3]
+    deck = Deck.new(cards)
+    round = Round.new(deck)
+
+    round.take_turn("Juneau")
+    expect(round.number_correct).to eq(1)
+    round.take_turn("Venus")
+    expect(round.number_correct).to eq(1)
+    round.take_turn("North north west")
+    expect(round.number_correct).to eq(2)
+  end
+
+ 
 
 end
