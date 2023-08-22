53bb6db2688fa08782eaa7a63df908db4ecb4093
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-08-21 22:19:27 -0600

    Add a third test to deck_spec and pass all tests

---
+++ lib/deck.rb
@@ -0,0 +1,12 @@
+class Deck
+  attr_reader :cards
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def cards_in_category(category)
+    cards.count do |card|
+      card.category == category
+    end
+  end
+end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,5 +1,5 @@
-require '.lib/deck'
-require '.lib/card'
+require './lib/deck'
+require './lib/card'
 
 card1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
 card2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
@@ -8,7 +8,7 @@ cards = [card1, card2, card3]
 
 RSpec.describe Deck do
   it 'exists' do
-    deck = Deck.new
+    deck = Deck.new(cards)
 
     expect(deck).to be_instance_of(Deck)
   end
@@ -16,9 +16,18 @@ RSpec.describe Deck do
   it 'can contain cards' do
     deck = Deck.new(cards)
 
-    deck.cards
     expect(deck.cards.count).to eq(3)
     expect(deck.cards[0]).to eq(card1)
   end
+
+  it 'can tell how many cards are in each category' do
+    deck = Deck.new(cards)
+
+    expect(deck.cards_in_category(:STEM)).to eq(2)
+    expect(deck.cards_in_category(:Geography)).to eq(1)
+    expect(deck.cards_in_category("Pop Culture")).to eq(0)
+  end
+
+
 
 end
